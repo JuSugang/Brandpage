ac1284a072c2d3140d43b755e763efa9629cbc7a
JuSugang <tnrkd7679@naver.com>
2017-01-25 15:54:00 +0000

    타이틀이미지,가로nav,아이콘변경

---
+++ app/assets/javascripts/sideNavbar.js
@@ -0,0 +1,8 @@
+function w3_open() {
+    document.getElementById("mySidenav").style.display = "block";
+    document.getElementById("sidenavIcon").style.display = "none";
+}
+function w3_close() {
+    document.getElementById("mySidenav").style.display = "none";
+    document.getElementById("sidenavIcon").style.display = "block";
+}
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brandpage::Application.config.secret_key_base = 'bb0dbce2349de25ff93bbcdd8a2b0ecc17b22b2f443d3f564fcf8b527c67c1690fbbab0c2405aa296fb861ef819c4ffc878dc724d3486e5204fdde354a0c208b'
